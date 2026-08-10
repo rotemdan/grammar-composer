f629b78e3404d7f06822b9793ca293ca33a41566
Rotem Dan <rotemdan@gmail.com>
2026-08-10 12:16:20 +0300

    Update test

--- src/tests/Test.ts
+++ src/tests/Test.ts
@@ -1,6 +1,6 @@
 import { Timer } from "../utilities/Timer.js"
 import { jsonSample1, jsonSample2 } from "./test-data/TestData.js"
-import { anyOf, buildGrammar } from "../parser-generator/GrammarComposer.js"
+import { anyOf, buildGrammar } from "../exports/Exports.js"
 import { JsonGrammar } from "./test-grammars/JsonGrammar.js"
 import { XmlGrammar } from "./test-grammars/XmlGrammar.js"
 import { RegExpGrammar } from "./test-grammars/RegExpGrammar.js"
